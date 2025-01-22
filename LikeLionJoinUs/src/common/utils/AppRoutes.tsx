9464ce2700d23e843f8c2f079484e73b3455c060
eunseo <jangeun4097@naver.com>
2025-01-22 12:48:46 +0900

    [feat] result페이지 반응형적용

--- LikeLionJoinUs/src/common/utils/AppRoutes.tsx
+++ LikeLionJoinUs/src/common/utils/AppRoutes.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../../doamin/home/Home';
-import NotFound from '../../doamin/shared/NotFound';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../../doamin/home/Home";
+import NotFound from "../../doamin/shared/NotFound";
+import Result from "../../doamin/result/Result";
 
 const AppRoutes: React.FC = () => {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRoutes;
