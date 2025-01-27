f67b62ff9cb90fb6580401579bca5850592f08a6
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-01-27 15:19:27 +0900

    [feat] palm 컴포넌트 구현

---
+++ LikeLionJoinUs/src/doamin/question/components/Palm.tsx
@@ -0,0 +1,11 @@
+export const BluePalm = () => {
+	return (
+		<div className="w-[19px] h-[19px] bg-[url('/assets/img/palms.png')] bg-[left_-87px_top_-52px] bg-no-repeat rotate-[90deg] p-3"></div>
+	);
+};
+
+export const GrayPalm = () => {
+	return (
+		<div className="w-[19px] h-[19px] bg-[url('/assets/img/palms.png')] bg-[left_-61px_top_-52px] bg-no-repeat rotate-[90deg] p-3"></div>
+	);
+};
