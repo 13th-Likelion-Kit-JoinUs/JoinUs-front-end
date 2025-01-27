7a0b4dbc3af332682b14f9d2601e23a5caf2791e
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-01-27 16:52:54 +0900

    [feat] 질문 페이지랑 로딩 페이지 라우팅 구현

--- LikeLionJoinUs/src/common/utils/AppRoutes.tsx
+++ LikeLionJoinUs/src/common/utils/AppRoutes.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../../doamin/home/Home';
 import NotFound from '../../doamin/shared/NotFound';
+import Question from '../../doamin/question/QuestionPage';
+import LoadingPage from '../../doamin/loading/LoadingPage';
 
 const AppRoutes: React.FC = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/question" element={<Question />} />
+				<Route path="/loading" element={<LoadingPage />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Router>
