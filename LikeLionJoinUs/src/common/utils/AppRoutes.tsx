738280181cc1fe16c163aa3f88ab10af339098b4
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-01-27 17:18:30 +0900

    [feat] 로딩 페이지 구현
    - 3초 뒤 ResultPage로 넘어가도록 구현

--- LikeLionJoinUs/src/common/utils/AppRoutes.tsx
+++ LikeLionJoinUs/src/common/utils/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from '../../doamin/home/Home';
 import NotFound from '../../doamin/shared/NotFound';
 import Question from '../../doamin/question/QuestionPage';
 import LoadingPage from '../../doamin/loading/LoadingPage';
+import ResultPage from '../../doamin/result/ResultPage';
 
 const AppRoutes: React.FC = () => {
 	return (
@@ -12,6 +13,7 @@ const AppRoutes: React.FC = () => {
 				<Route path="/" element={<Home />} />
 				<Route path="/question" element={<Question />} />
 				<Route path="/loading" element={<LoadingPage />} />
+				<Route path="/result" element={<ResultPage />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Router>
