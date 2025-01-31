29f900286d3eb228dc42d0f3035c16664481e50c
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-01-31 21:05:19 +0900

    [feat] query로 결과 정보 전달 기능 추가

--- LikeLionJoinUs/src/doamin/loading/LoadingPage.tsx
+++ LikeLionJoinUs/src/doamin/loading/LoadingPage.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import TextBoard from '../home/components/TextBoard';
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+type scoretype = {
+	scorePlanning: number;
+	scoreDesign: number;
+	scoreFront: number;
+	scoreBack: number;
+};
 
 const LoadingPage = () => {
+	const location = useLocation();
+	const scores: scoretype = location.state.value;
 	const navigate = useNavigate();
+
+	const maxScore = Math.max(...Object.values(scores));
+
+	const normalizedScores = Object.fromEntries(
+		Object.entries(scores).map(([key, value]) => [
+			key,
+			Math.floor((value / maxScore) * 5),
+		]),
+	);
+
+	console.log(normalizedScores);
+
 	setTimeout(() => {
-		navigate('/result');
+		navigate(
+			`/result?planning=${normalizedScores.scorePlanning}&analysis=${normalizedScores.scoreBack}&practice=${normalizedScores.scoreFront}&creative=${normalizedScores.scoreDesign}`,
+		);
 	}, 3000);
+
 	return (
 		<div className="flex flex-col h-screen items-center justify-around">
 			<img
