6c0525ed994af647866be444763cc8d61f489074
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-09 14:08:36 +0900

    [style] infoListItem 컴포넌트 재사용성 수정

--- LikeLionJoinUs/src/doamin/result/components/InfoListItem.tsx
+++ LikeLionJoinUs/src/doamin/result/components/InfoListItem.tsx
@@ -1,14 +1,15 @@
 export const InfoListItem = ({
-  label,
-  items,
+	label,
+	items,
 }: {
-  label: string;
-  items: string[];
+	label: string;
+	items: string[];
 }) => (
-  <div className="relative grid pl-4 mb-2">
-    <span className="absolute left-0 top-0 bg-[url('/~s20231073/assets/img/right_arrow_black.png')] w-[7px] h-[10px] bg-cover mt-1" />
-    <span className="text-md leading-tight break-words">
-      {label}: {items.join(", ")}
-    </span>
-  </div>
+	<div className="relative grid pl-4 mb-2">
+		<span className="absolute left-0 top-0 bg-[url('/~s20231073/assets/img/right_arrow_black.png')] w-[7px] h-[10px] bg-cover mt-1" />
+		<span className="text-md leading-tight break-words">
+			{label ? `${label}: ` : ''}
+			{items.join(', ')}
+		</span>
+	</div>
 );
