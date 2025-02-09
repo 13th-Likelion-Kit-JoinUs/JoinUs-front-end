d845a2684e013f1b2a59616204913edcb20acc23
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-09 14:25:18 +0900

    [fix] Z Flip 5 사이즈 호환되도록 사이즈 조정

--- LikeLionJoinUs/src/doamin/question/components/ChoiceBoard.tsx
+++ LikeLionJoinUs/src/doamin/question/components/ChoiceBoard.tsx
@@ -6,11 +6,11 @@ type message = {
 const ChoiceBoard = ({ data, onClick }: message): JSX.Element => {
 	return (
 		<div
-			className="bg-[url('/~s20231073/assets/img/ballon_wide.png')] w-[23rem] h-[7.5rem] bg-cover flex flex-row items-center justify-center"
+			className="bg-[url('/~s20231073/assets/img/ballon_wide.png')] w-[20rem] h-[6.4rem] bg-cover flex flex-row items-center justify-center"
 			onClick={onClick}
 		>
 			<span className="bg-[url('/~s20231073/assets/img/right_arrow_black.png')] w-[0.5rem] h-[0.7rem] bg-cover mr-4"></span>
-			<div className="text-[0.9em] w-[16rem]">
+			<div className="text-[0.8em] w-[16rem]">
 				{data.split('\n').map((line, index) => (
 					<span key={index}>
 						{line}
