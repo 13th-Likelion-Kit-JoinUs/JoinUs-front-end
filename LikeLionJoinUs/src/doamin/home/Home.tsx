d150ddc64f588776914f94ca9481fe16e7d7b291
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-16 12:36:02 +0900

    [Fix] 메인 페이지 테스트하기 버튼 활성화

--- LikeLionJoinUs/src/doamin/home/Home.tsx
+++ LikeLionJoinUs/src/doamin/home/Home.tsx
@@ -24,29 +24,31 @@ const Home = () => {
 				</div>
 			</div>
 			<div className="flex justify-center m-8">
-				<div
-					className="bg-[url('/~s20231073/assets/img/ballon_box.png')] 
+				<Link to="/question">
+					<div
+						className="bg-[url('/~s20231073/assets/img/ballon_box.png')] 
                 w-[9rem] h-[10rem] sm:w-[9rem] sm:h-[10rem] md:w-[11rem] md:h-[12rem] 
                 bg-cover flex flex-col items-center justify-center pb-6"
-				>
-					<div className="text-[0.9em] p-2 ">
-						나는 어떤 <br />
-						아기사자가 될까?
-					</div>
-					<div
-						className="bg-[url('/~s20231073/assets/img/start_button.png')] 
+					>
+						<div className="text-[0.9em] p-2 ">
+							나는 어떤 <br />
+							아기사자가 될까?
+						</div>
+						<div
+							className="bg-[url('/~s20231073/assets/img/start_button.png')] 
                     w-[4rem] h-[1.4rem] sm:w-[5rem] sm:h-[1.8rem] md:w-[6rem] md:h-[2rem] 
                     bg-contain text-[0.6em] sm:text-[0.7em] md:text-[0.8em] 
                     m-1 text-center bg-bl text-white flex flex-row items-center justify-center"
-					>
-						<span
-							className="bg-[url('/~s20231073/assets/img/right_arrow_white.png')] 
+						>
+							<span
+								className="bg-[url('/~s20231073/assets/img/right_arrow_white.png')] 
                         w-[0.5rem] h-[0.6rem] sm:w-[0.5rem] sm:h-[0.5rem] md:w-[0.6rem] md:h-[0.6rem] 
                         bg-cover mr-1"
-						></span>
-						<span>테스트</span>
+							></span>
+							<span>테스트</span>
+						</div>
 					</div>
-				</div>
+				</Link>
 
 				<div className="bg-[url('/~s20231073/assets/img/main_lion.png')] w-[10rem] h-[14rem] bg-cover"></div>
 			</div>
