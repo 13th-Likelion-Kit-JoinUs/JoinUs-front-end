d4be72d0a7df89d10dc476038bcbafe5fbc20ccc
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-01 11:16:01 +0900

    [style] Question 페이지 반응형 작업

--- LikeLionJoinUs/src/doamin/question/components/ChoiceBoard.tsx
+++ LikeLionJoinUs/src/doamin/question/components/ChoiceBoard.tsx
@@ -6,17 +6,12 @@ type message = {
 const ChoiceBoard = ({ data, onClick }: message): JSX.Element => {
 	return (
 		<div
-			className="bg-[url('/~s20231073/assets/img/ballon_wide.png')] w-[20rem] h-[6.4rem] bg-cover flex flex-row items-center justify-center"
+			className="bg-[length:100%_100%] bg-[url('/~s20231073/assets/img/ballon_wide.png')] w-[20rem] h-[6rem] flex flex-row items-center justify-center medium:w-[26rem] medium:h-[7rem] tablet:w-[33rem] tablet:h-[8rem]"
 			onClick={onClick}
 		>
 			<span className="bg-[url('/~s20231073/assets/img/right_arrow_black.png')] w-[0.5rem] h-[0.7rem] bg-cover mr-4"></span>
-			<div className="text-[0.8em] w-[16rem]">
-				{data.split('\n').map((line, index) => (
-					<span key={index}>
-						{line}
-						<br />
-					</span>
-				))}
+			<div className="text-[0.8em] w-[16rem] medium:w-[20rem] medium:text-[1em] tablet:w-[23rem] tablet:text-[1.2em]">
+				{data}
 			</div>
 		</div>
 	);
--- LikeLionJoinUs/src/doamin/question/components/Palm.tsx
+++ LikeLionJoinUs/src/doamin/question/components/Palm.tsx
@@ -1,17 +1,25 @@
 export const BluePalm = () => {
-  return (
-    <div className="w-[19px] h-[19px] bg-[url('/~s20231073/assets/img/palms.png')] bg-[left_-87px_top_-52px] bg-no-repeat rotate-[90deg] p-3"></div>
-  );
+	return (
+		<img
+			src="assets/img/blue_palm.svg"
+			className="w-[25px] h-auto medium:w-[30px] tablet:w-[38px]"
+		/>
+		// <div className="w-[17px] h-[17px] bg-[url('/~s20231073/assets/img/blue_palm.svg')] bg-no-repeat p-3"></div>
+	);
 };
 
 export const GrayPalm = () => {
-  return (
-    <div className="w-[19px] h-[19px] bg-[url('/~s20231073/assets/img/palms.png')] bg-[left_-61px_top_-52px] bg-no-repeat rotate-[90deg] p-3"></div>
-  );
+	return (
+		<img
+			src="assets/img/gray_palm.svg"
+			className="w-[25px] h-auto medium:w-[30px] tablet:w-[38px]"
+		/>
+		// <div className="w-[22px] h-[22px] bg-[url('/~s20231073/assets/img/gray_palm.svg')] p-3"></div>
+	);
 };
 
 export const BluePalmUp = () => {
-  return (
-    <div className="w-[19px] h-[19px] mt-1 bg-[url('/~s20231073/assets/img/palms.png')] bg-[left_-87px_top_-52px] bg-no-repeat  p-3"></div>
-  );
+	return (
+		<div className="w-[19px] h-[19px] mt-1 bg-[url('/~s20231073/assets/img/palms.png')] bg-[left_-87px_top_-52px] bg-no-repeat  p-3"></div>
+	);
 };
