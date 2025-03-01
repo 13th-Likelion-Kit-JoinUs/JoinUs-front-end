648aa3628c8558ad8d7aec7f6f47646798e0ece0
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-01 11:20:10 +0900

    [Style] Home 페이지 반응형 재수정

--- LikeLionJoinUs/src/doamin/home/components/TextBoard.tsx
+++ LikeLionJoinUs/src/doamin/home/components/TextBoard.tsx
@@ -4,8 +4,8 @@ type textProps = {
 
 const TextBoard = ({ text }: textProps): JSX.Element => {
 	return (
-		<div className="bg-[url('/~s20231073/assets/img/ballon_wide.png')]  w-[20rem] h-[6.4rem] bg-cover flex flex-row items-center justify-center  medium:bg-[url('assets/img/main_loading_box_480.svg')] medium:w-[29rem] tablet:bg-[url('assets/img/main_loading_box_768.svg')] tablet:w-[35rem]">
-			<div className="text-[1em] w-[14rem] medium:text-[1.4em] medium:w-[14em] tablet:text-[1.4em] tablet:w-[17em]">
+		<div className="bg-[length:100%_100%] bg-[url('/~s20231073/assets/img/ballon_wide.png')]  w-[20rem] h-[6.4rem] flex flex-row items-center justify-center medium:w-[29rem] medium:h-[8rem] tablet:w-[35rem] tablet:h-[10rem]">
+			<div className="text-[1em] w-[14rem] medium:text-[1.2em] medium:w-[16em] tablet:text-[1.4em] tablet:w-[17em]">
 				{text.split("\n").map((line, index) => (
 					<span key={index}>
 						{line}
@@ -15,8 +15,7 @@ const TextBoard = ({ text }: textProps): JSX.Element => {
 			</div>
 			<div
 				className="bg-[url('/~s20231073/assets/img/right_arrow_black.png')]  
-				rotate-90 w-[0.6rem] h-[0.9rem] bg-cover ml-10 mt-4
-				medium:hidden tablet:hidden"
+				rotate-90 w-[0.6rem] h-[0.9rem] bg-cover ml-10 mt-4"
 			></div>
 		</div>
 	);
