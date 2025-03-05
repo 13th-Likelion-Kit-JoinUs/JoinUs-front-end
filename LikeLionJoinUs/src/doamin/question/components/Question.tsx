c9f4b06ad5f70e791f26eca2a529a2582169c7cc
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-05 19:39:59 +0900

    [Design] 질문지 글자 여백 수정

--- LikeLionJoinUs/src/doamin/question/components/Question.tsx
+++ LikeLionJoinUs/src/doamin/question/components/Question.tsx
@@ -4,8 +4,15 @@ type QuestionProps = {
 
 const Question = ({ data }: QuestionProps): JSX.Element => {
 	return (
-		<div className="m-3 text-[1.2em] w-[18rem] medium:w-[25rem] medium:text-[1.5em] tablet:w-[28rem] tablet:text-[2rem]">
-			<h1>{data}</h1>
+		<div className="m-3 text-[1.2em] w-[22rem] medium:w-[26rem] medium:text-[1.5em] tablet:w-[36rem] tablet:text-[2rem]">
+			<h1 className="break-words text-center">
+				{data.split("\n").map((line, index) => (
+					<span key={index}>
+						{line}
+						<br />
+					</span>
+				))}{" "}
+			</h1>
 		</div>
 	);
 };
